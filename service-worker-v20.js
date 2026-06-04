436877ed50bbd32fbb1c731de6fc0b0641b9f0aa
bonnie-svg <bonnie@trappedupstate.com>
2026-06-04 15:40:02 -0400

    Add files via upload

---
+++ service-worker-v20.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "escape-room-phone-v20";
+const FILES = [
+  "./",
+  "./index.html",
+  "./styles-v20.css",
+  "./app-v20.js",
+  "./manifest.webmanifest",
+  "./assets/icon.svg",
+  "./assets/correct-call.wav",
+  "./assets/wrong-number.wav",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(FILES))
+      .then(() => self.skipWaiting()),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((names) => Promise.all(
+        names.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name)),
+      ))
+      .then(() => self.clients.claim()),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request)),
+  );
+});
