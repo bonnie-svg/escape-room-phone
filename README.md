16e2827226c53e7e28a1d8c4f3c81312a5dd10b4
bonnie-svg <bonnie@trappedupstate.com>
2026-06-03 20:56:32 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,24 @@
+# Escape Room Phone
+
+Open `index.html` on a phone or host this folder on a local web server. The default correct number is:
+
+```text
+5552328057
+```
+
+To change it, edit `app.js` and replace this line:
+
+```js
+correctNumber: "5552328057",
+```
+
+Put your recordings in the `assets` folder using these exact filenames:
+
+```text
+assets/correct-call.mp3
+assets/wrong-number.mp3
+```
+
+The app expects MP3 audio files.
+
+On iPhone or Android, open the page in the browser and choose the browser option to add it to the Home Screen. It will launch like a standalone phone prop.
---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "escape-room-phone-v14";
+const FILES = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./assets/icon.svg",
+  "./assets/correct-call.wav",
+  "./assets/wrong-number.wav",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(FILES))
+      .then(() => self.skipWaiting()),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((names) => Promise.all(
+        names.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name)),
+      ))
+      .then(() => self.clients.claim()),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request)),
+  );
+});
